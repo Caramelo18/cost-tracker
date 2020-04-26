8bbeb39ae9ed618b08467cdbcbea8f82fba4263e
Caramelo18 <fabiocaramelo18@gmail.com>
2020-04-26 17:10:55 +0200

    use useMemo and style adjustments

--- client/src/components/budget/chart.tsx
+++ client/src/components/budget/chart.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import { Chart } from 'react-charts'
 
+import { groupTransactionsByMonth } from '../../utils/utils';
+
 function BudgetChart(props: any) {
-    let data = [
-          {
-            label: 'Needs',
-            data: props.data.needs
-          },
-          {
-            label: 'Wants',
-            data: props.data.wants
-          },
-          {
-            label: 'Credit',
-            data: props.data.credit
-          },
-        ];
+    const data = React.useMemo(() => getChartData(props.transactions, props.monthsToAnalyze), [props.transactions, props.monthsToAnalyze]);
 
     const axes = React.useMemo(
         () => [
@@ -34,3 +23,68 @@ function BudgetChart(props: any) {
   }
 
   export default BudgetChart;
+
+
+function getChartData(transactions: any[], monthsToAnalyze: number) {
+    const transactionsByMonth = groupTransactionsByMonth(transactions);
+
+    let months = Object.keys(transactionsByMonth);
+
+    let monthsResult = {};
+
+    for (const month of months) {
+        monthsResult[month] = groupTransactions(transactionsByMonth[month]);
+    }
+
+    months = months.slice(0, monthsToAnalyze).reverse();
+    
+    let needs: any[] = [], wants: any[] = [], credit: any[] = [];
+
+    for (const month of months) {
+        if ('Needs' in monthsResult[month]) {
+            needs.push([month, monthsResult[month].Needs.ammount]);
+        }
+        else {
+            needs.push([month, 0]);
+        }
+        if ('Wants' in monthsResult[month]) {
+            wants.push([month, monthsResult[month].Wants.ammount]);
+        } else {
+            wants.push([month, 0]);
+        }
+        if ('Credit' in monthsResult[month]) {
+            credit.push([month, monthsResult[month].Credit.ammount]);
+        } else {
+            credit.push([month, 0]);
+        }
+    }
+    
+    return [
+        {
+          label: 'Needs',
+          data: needs
+        },
+        {
+          label: 'Wants',
+          data: wants
+        },
+        {
+          label: 'Credit',
+          data: credit
+        },
+      ];
+}
+
+function groupTransactions(transactions: any[]) {
+    return transactions.reduce((accumulator: any, currTransaction: any) => {
+        const category = currTransaction.category;
+        
+        if(category in accumulator) {
+            accumulator[category]['count'] += 1;
+            accumulator[category]['ammount'] += Math.abs(currTransaction.value);
+        } else {
+            accumulator[category] = { 'count': 1, 'ammount': Math.abs(currTransaction.value)};
+        }
+        return accumulator;
+    }, Object.create(null));
+}
